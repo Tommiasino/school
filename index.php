adae8c4bad04474b929361ced00965fec32f665a
Tommiasino <56842739+Tommiasino@users.noreply.github.com>
2019-10-21 19:59:14 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'kuzel.php';
+
+$k1=new kuzel(565,50);
+$k1->info();
---
+++ kuzel.php
@@ -0,0 +1,44 @@
+<?php
+require 'iteleso.php';
+class kuzel implements ITeleso{
+    public $r;
+    public $v;
+    public $cornerCount=1;
+   
+
+    public function __construct($r, $v){
+        $this->r=$r;
+        $this->v=$v; 
+    }
+    
+    public function info() {
+            echo "poloměr podstavy je ".$this->getR()."<br>výška kuželu je ".$this->v;
+            echo "<br>povrch kuželu je ".$this->povrch()."<br>objem kuželu je ".$this->objem(); 
+        
+    }
+
+    public function is3D() {
+        return TRUE;
+    }
+
+    public function objem() {
+        return ((M_PI*$this->getR()*$this->getR())*$this->getV())/3;
+    }
+
+    public function pocetVrcholu() {
+        return $this->cornerCount;
+    }
+
+    public function povrch() {
+        $s= sqrt($this->getR()*$this->getR()+$this->getV()*$this->getV());
+       return M_PI*$this->getR()*$this->getR()+M_PI*$this->getR()*$s;
+    }
+    public function getR(){
+        return $this->r;
+        
+    }
+    public function getV(){
+        return $this->v;
+        
+    }
+}
